7576e5e3025e13c4ddffab53b692c6ecaa12882a
bot2476 <157342667+bot2476@users.noreply.github.com>
2024-01-23 13:21:30 +0800

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -17,7 +17,7 @@ this will help you to remove a indexed channel from your group.
 
 Get connected channels list with - /connections"""
 
-    ABOUT = """Developed By @Movie_service
+    ABOUT = """Developed By @CyniteBackup
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
 ✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
